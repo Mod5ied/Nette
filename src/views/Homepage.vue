027c426b8d137eb00219d292adfc14b47aa421f9
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2021-12-04 14:03:24 +0100

    Updates to Homepage:
    Finalized the alignments for Desktop and Tablet views.
    Removed images and replaced with fonts from F.A.
    Updates to styles and nifties

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,52 +1,49 @@
 <template>
   <div id="main">
     <Header :title="title" />
-    <section id="sec1">
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est a dolorum
-        repellendus asperiores, tempora quidem nostrum accusamus repellat cum
-        sapiente. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-        et temporibus atque ad aliquid consequatur veritatis possimus reiciendis
-        distinctio repellendus?
-      </p>
-    </section>
-    <section class="image" id="mobilehidden">
-      <img src="../assets/IMG_20200701_225758.jpg" alt="" />
-    </section>
-    <section id="sec2">
-      <div class="cardCover one">
-        <img src="../assets/palette_black_24dp.svg" class="image1" />
-        <span class="imageDesc">ART</span>
-      </div>
-      <div class="cardCover two">
-        <img src="../assets/pets_black_24dp.svg" class="image1" />
-        <span class="imageDesc">PETS</span>
-      </div>
-      <div class="cardCover three">
-        <img src="../assets/houses_black_24dp.svg" class="image1" />
-        <span class="imageDesc">HOUSES</span>
-      </div>
-      <div class="cardCover">
-        <img src="../assets/park_black_24dp.svg" class="image1" />
-        <span class="imageDesc">PLANTS</span>
-      </div>
-      <div class="cardCover four">
-        <ion-icon name="logo-model-s" id="image"></ion-icon>
-        <span class="imageDesc">CARS</span>
-      </div>
-    </section>
-    <section id="sec3">
-      <h2>Lorem ipsum dolor, sit amet caonsectetur.</h2>
-      <div class="stack">
-        <!-- <img src="../assets/paint_logo-11.jpg" class="image2" /> -->
-        <li class="image2 one">Image1</li>
-        <li class="image2 two">Image2</li>
-        <li class="image2 three mobilehidden">Image3</li>
-        <li class="image2 four mobilehidden">Image4</li>
-        <li class="image2 five mobilehidden">Image5</li>
-        <li class="image2 six mobilehidden">Image6</li>
-      </div>
-    </section>
+      <section id="sec1">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est a
+          dolorum repellendus asperiores, tempora quidem nostrum accusamus
+          repellat cum sapiente.
+        </p>
+      </section>
+      <section class="image" id="mobilehidden">
+        <img src="../assets/IMG_20200701_225758.jpg" alt="" />
+      </section>
+      <section id="sec2">
+        <div class="cardCover one">
+          <i class="fas fa-palette image1"></i>
+          <span class="imageDesc">ART</span>
+        </div>
+        <div class="cardCover two">
+          <i class="fas fa-paw image1"></i>
+          <span class="imageDesc">PETS</span>
+        </div>
+        <div class="cardCover three">
+          <i class="far fa-building image1"></i>
+          <span class="imageDesc">HOUSES</span>
+        </div>
+        <div class="cardCover">
+          <i class="fab fa-pagelines image1"></i>
+          <span class="imageDesc">PLANTS</span>
+        </div>
+        <div class="cardCover four">
+          <i class="fas fa-car image1"></i>
+          <span class="imageDesc">CARS</span>
+        </div>
+      </section>
+      <section id="sec3">
+        <h2>Exhibition</h2>
+        <div class="stack">
+          <li class="image2 one">Image1</li>
+          <li class="image2 two">Image2</li>
+          <li class="image2 three mobilehidden">Image3</li>
+          <li class="image2 four mobilehidden">Image4</li>
+          <li class="image2 five mobilehidden">Image5</li>
+          <li class="image2 six mobilehidden">Image6</li>
+        </div>
+      </section>
     <Footer />
   </div>
 </template>
